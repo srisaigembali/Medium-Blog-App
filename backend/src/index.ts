f5567eea354d5ce62c6993f153954e878e98e552
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2024-10-01 11:39:18 +0530

    added prisma postgres

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,18 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string;
+	};
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	}).$extends(withAccelerate());
+	return c.text("Hello Hono!");
+});
 
-export default app
+export default app;
